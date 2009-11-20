1ae97d32b642ff1f07c1374e60b1071ed456c3bc
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-11-20 10:38:21 +0000

    * added missing copyright comments

--- controller/TodoyuSearchSearchresultsActionController.class.php
+++ controller/TodoyuSearchSearchresultsActionController.class.php
@@ -1,7 +1,26 @@
 <?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 snowflake productions gmbh
+*  All rights reserved
+*
+*  This script is part of the todoyu project.
+*  The todoyu project is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License, version 2,
+*  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
+*  the Free Software Foundation;
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
 
 class TodoyuSearchSearchresultsActionController extends TodoyuActionController {
-	
+
 	public function updateAction(array $params) {
 		$tab			= $params['tab'];
 		$idFilterset	= intval($params['filterset']);
@@ -13,8 +32,8 @@ class TodoyuSearchSearchresultsActionController extends TodoyuActionController {
 			return TodoyuFilterAreaRenderer::renderResults($tab, $idFilterset, $conditions, $conjunction);
 		}
 	}
-	
-	
+
+
 }
 
 ?>
