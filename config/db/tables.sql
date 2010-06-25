56644e52795a7038ff8a8c701f6f0d1fdd553525
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-06-25 09:38:13 +0000

    * adjusted all SQL files to comply with coding convention

--- config/db/tables.sql
+++ config/db/tables.sql
@@ -3,16 +3,16 @@
 --
 
 CREATE TABLE `ext_search_filtercondition` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `date_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `date_update` int(10) unsigned NOT NULL,
-  `id_person_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `deleted` tinyint(2) NOT NULL DEFAULT '0',
-  `id_set` int(10) unsigned NOT NULL,
-  `filter` varchar(64) NOT NULL,
-  `value` varchar(100) NOT NULL,
-  `negate` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`date_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`date_update` int(10) unsigned NOT NULL,
+	`id_person_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`deleted` tinyint(2) NOT NULL DEFAULT '0',
+	`id_set` int(10) unsigned NOT NULL,
+	`filter` varchar(64) NOT NULL,
+	`value` varchar(100) NOT NULL,
+	`negate` tinyint(1) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
 -- --------------------------------------------------------
@@ -22,16 +22,16 @@ CREATE TABLE `ext_search_filtercondition` (
 --
 
 CREATE TABLE `ext_search_filterset` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `date_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `date_update` int(10) unsigned NOT NULL,
-  `id_person_create` int(10) unsigned NOT NULL DEFAULT '0',
-  `deleted` tinyint(2) NOT NULL DEFAULT '0',
-  `sorting` int(10) unsigned NOT NULL,
-  `is_hidden` tinyint(2) NOT NULL DEFAULT '0',
-  `roles` varchar(16) NOT NULL,
-  `type` varchar(16) NOT NULL,
-  `title` varchar(64) NOT NULL,
-  `conjunction` varchar(3) NOT NULL,
-  PRIMARY KEY (`id`)
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`date_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`date_update` int(10) unsigned NOT NULL,
+	`id_person_create` int(10) unsigned NOT NULL DEFAULT '0',
+	`deleted` tinyint(2) NOT NULL DEFAULT '0',
+	`sorting` int(10) unsigned NOT NULL,
+	`is_hidden` tinyint(2) NOT NULL DEFAULT '0',
+	`roles` varchar(16) NOT NULL,
+	`type` varchar(16) NOT NULL,
+	`title` varchar(64) NOT NULL,
+	`conjunction` varchar(3) NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
