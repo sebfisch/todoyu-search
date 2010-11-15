12c8923715d3ce79a7221e2c033c0b806434bdde
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-11-15 14:57:01 +0000

    * Modified loading structure of todoyu to prevent failing rights checks on cookie login

--- config/load.php
+++ config/load.php
@@ -18,5 +18,6 @@
 * This copyright notice MUST APPEAR in all copies of the script.
 *****************************************************************************/
 
+TodoyuHookManager::registerHook('core', 'requestVars', 'TodoyuBrowserSearch::hookNotLoggedIn', 980);
 
 ?>
