fc521a62b9519c4c9635d2a30b87125a68bc4ff5
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2011-04-18 14:34:32 +0000

    * cleanups

--- model/TodoyuSearchActionPanelManager.class.php
+++ model/TodoyuSearchActionPanelManager.class.php
@@ -18,18 +18,16 @@
 * This copyright notice MUST APPEAR in all copies of the script.
 *****************************************************************************/
 
-
 /**
- * @todo		comment
- *
  * @package		Todoyu
  * @subpackage	Search
  */
 class TodoyuSearchActionPanelManager {
 
 	/**
-	 * @todo	comment
+	 * Render action panel HTML
 	 *
+	 * @param	String	$activeTab		e.g. 'task' / 'project'
 	 * @return	String
 	 */
 	public static function renderActionPanel($activeTab) {
@@ -45,14 +43,16 @@ class TodoyuSearchActionPanelManager {
 
 
 	/**
-	 * @todo	comment
+	 * Export result items of given filter conditions with registered export function
 	 *
-	 * @param	$name
-	 * @param	$tab
-	 * @param	$params
+	 * @param	String		$name
+	 * @param	String		$type			'task' / 'project' etc.
+	 * @param	Integer		$idFilterset
+	 * @param	Array		$conditions
+	 * @param	String		$conjunction	logical conjunction ('AND' / 'OR')
 	 */
-	public static function dispatchExport($name, $type, $idFilterset, $conditions, $conjunction = array()) {
-		$export	= self::getExport($type, $name);
+	public static function dispatchExport($name, $type, $idFilterset, $conditions, $conjunction) {
+		$export	= self::getExportOfTypeAndName($type, $name);
 
 		$idFilterset	= intval($idFilterset);
 		$conjunction	= strtoupper($conjunction) === 'OR' ? 'OR' : 'AND';
@@ -88,14 +88,17 @@ class TodoyuSearchActionPanelManager {
 
 
 	/**
-	 * @todo	comment
+	 * Add result items type to search action panel
 	 *
-	 * @param	$type
-	 * @param	$name
-	 * @param	$method
+	 * @param	String	$type
+	 * @param	String	$name
+	 * @param	String	$callback
+	 * @param	String	$label
+	 * @param	String	$htmlClass
+	 * @param	String	$right
 	 */
-	public static function addExport($type, $name, $method, $label, $htmlClass = '', $right = '') {
-		if(strlen($right) != '') {
+	public static function addExport($type, $name, $callback, $label, $htmlClass = '', $right = '') {
+		if( strlen($right) != '' ) {
 			$rightArray = explode(':', $right);
 			$rightExt	= $rightArray[0];
 			unset($rightArray[0]);
@@ -103,7 +106,7 @@ class TodoyuSearchActionPanelManager {
 		}
 
 		Todoyu::$CONFIG['EXT']['search']['filter'][$type]['actionpanel']['export'][$name] = array(
-			'method'	=> $method,
+			'method'	=> $callback,
 			'htmlClass'	=> ($htmlClass ? $htmlClass : $name),
 			'label'		=> $label,
 			'right'		=> array('ext'	=> $rightExt,
@@ -114,20 +117,20 @@ class TodoyuSearchActionPanelManager {
 
 
 	/**
-	 * @todo	comment
+	 * Get export configuration of given type/name
 	 *
-	 * @param	$type
-	 * @param	$name
-	 * @return	String
+	 * @param	String		$type
+	 * @param	String		$name
+	 * @return	Array
 	 */
-	public static function getExport($type, $name) {
+	public static function getExportOfTypeAndName($type, $name) {
 		return Todoyu::$CONFIG['EXT']['search']['filter'][$type]['actionpanel']['export'][$name];
 	}
 
 
 
 	/**
-	 * @todo	comment
+	 * Get type export configuration
 	 *
 	 * @param	String	$type
 	 * @return	String
