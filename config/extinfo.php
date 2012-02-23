103cc7d42012199510a67f06a7cb7b2f0878c985
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2012-02-23 09:31:09 +0000

    * updated changelogs + version numbers

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,7 +31,7 @@ Todoyu::$CONFIG['EXT']['search']['info'] = array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state'			=> 'stable',
-	'version'		=> '1.2.0',
+	'version'		=> '1.3.1',
 	'constraints' => array(
 		'core'		=> '2.1.4',
 		'depends'	=> array(),
