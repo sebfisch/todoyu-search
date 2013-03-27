a08e9d6bff9593dc610737981bef0a8ad3533beb
nkarrer <nkarrer@7f067039-fbb2-4aac-a504-d491c780d6a3>
2013-03-27 10:15:55 +0000

    * Adapted ext-config: versions & dependencies

--- config/extinfo.php
+++ config/extinfo.php
@@ -33,7 +33,7 @@ Todoyu::$CONFIG['EXT']['search']['info'] = array(
 	'state'			=> 'stable',
 	'version'		=> '1.5.0',
 	'constraints' => array(
-		'core'		=> '2.3.0b1',
+		'core'		=> '2.3.0',
 		'system'	=> true
 	),
 	'urlDocumentation'	=> 'http://doc.todoyu.com/?search'
